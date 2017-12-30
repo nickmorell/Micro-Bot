3405afd45530e64aab3fecb4d94c241baecdfe76
nickmorell <cyclones1>
2017-12-29 22:32:40 -0500

    Starting switch to base Discord js

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-const { CommandoClient } = require('discord.js-commando');
-const path = require('path');
+// const { CommandoClient } = require('discord.js-commando');
+// const path = require('path');
     
-const client = new CommandoClient({
-    commandPrefix: '!',
-    unknownCommandResponse: false,
-    owner: process.env.OWNER_ID,
-    disableEveryone: true
-});
+// const client = new CommandoClient({
+//     commandPrefix: '!',
+//     unknownCommandResponse: false,
+//     owner: process.env.OWNER_ID,
+//     disableEveryone: true
+// });
 
-client.registry
-    .registerDefaultTypes()
-    .registerGroups([
-        ['basic', 'Our Basic Command Group']
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands()
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+// client.registry
+//     .registerDefaultTypes()
+//     .registerGroups([
+//         ['basic', 'Our Basic Command Group']
+//     ])
+//     .registerDefaultGroups()
+//     .registerDefaultCommands()
+//     .registerCommandsIn(path.join(__dirname, 'commands'));
+
+// client.on('ready', () => {
+//     console.log('Logged in!');
+//     client.user.setGame('Twitch Tv');
+// });
+
+// client.login(process.env.BOT_TOKEN);
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+let ownerID = process.env.OWNER_ID
 
 client.on('ready', () => {
-    console.log('Logged in!');
-    client.user.setGame('Twitch Tv');
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
